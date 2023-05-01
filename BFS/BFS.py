68d7b58d906e91334841084029a435ccb77ac2d7
criling <kim980316@naver.com>
2023-05-02 00:35:54 +0900

    first coding test commit

---
+++ BFS/BFS.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def bfs(graph, start_node):
+    need_visited, visited = list(), list()
+
+    need_visited.append(start_node)
+
+    while need_visited:
+        node = need_visited[0]
+        del need_visited[0]
+
+        if node not in visited:
+            visited.append(node)
+            need_visited.extend(graph[node])
+    return visited
+
+graph = dict()
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(bfs(graph, 'A'))
